c9488f7094a33c5e5387db7ded718b86d9bf73ff
samrussell-code <66648381+samrussell-code@users.noreply.github.com>
2021-10-20 00:26:17 +0100

    tommy is going to have a nice sleep

--- A-Level Project/1_launch_menu/account_server.py
+++ A-Level Project/1_launch_menu/account_server.py
@@ -1,22 +1,35 @@
-def REGISTER_ACCOUNT(inf):
-    username,password=inf
-    print(username,password)
+import sqlite3
+
+def dbConnect(filename):
+    print('Connecting to',filename,'...')
+    connection=sqlite3.connect(str(filename+'.db'))
+    return connection
+
+def dbCreateTable(connection,create_table_sql):#only run this to create a new table in the db
+    cursor=connection.cursor()
+    cursor.execute(create_table_sql)
+
+def REGISTER_ACCOUNT(username,password,cursor):
     return
-def LOGIN_ACCOUNT(inf):
-    username,password=inf
-    print(username,password)
+def LOGIN_ACCOUNT(username,password,cursor):
     return
-def ERROR_CATCH(id):
+def ERR_CATCH(id):
     errmsgs=[
-        'Unknown error',
-        'Unsupported opcode from client'
+        'Unknown error.',
+        'Unsupported opcode from client.',
+        'Account with this username already exists.',
+        'Cannot create the database connection.'
     ]
     print(str('Error #'+str(id)+'|'+errmsgs[id]+'\n'))
     return
 
-recv_opcode=8
-recv_operand=('John Smith','password123')
+recv_opcode,recv_operand=0,('John Smith','password123')#temporary data
+(username,password),connection=recv_operand,dbConnect('ACCOUNTS')
+sql_create_accounts_table="""CREATE TABLE IF NOT EXISTS profiles (
+    id integer PRIMARY KEY,
+    username text NOT NULL,
+    password text NOT NULL
+);"""
 
-REGISTER_ACCOUNT(recv_operand) if recv_opcode==0 else LOGIN_ACCOUNT(recv_operand) if recv_opcode==1 else ERROR_CATCH(1)
-
-
+dbCreateTable(connection, sql_create_accounts_table) if connection is not None else ERR_CATCH(3)
+REGISTER_ACCOUNT(username,password,connection) if recv_opcode==0 else LOGIN_ACCOUNT(username,password,connection) if recv_opcode==1 else ERR_CATCH(1)
