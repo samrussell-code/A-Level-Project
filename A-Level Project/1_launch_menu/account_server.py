b586ea84214a6da11c8ffe3406bd4e45c7c300af
samrussell-code <66648381+samrussell-code@users.noreply.github.com>
2021-10-19 22:51:47 +0100

    fixed it?

--- A-Level Project/1_launch_menu/account_server.py
+++ A-Level Project/1_launch_menu/account_server.py
@@ -1,3 +1,22 @@
-import socket,threading,time
+def REGISTER_ACCOUNT(inf):
+    username,password=inf
+    print(username,password)
+    return
+def LOGIN_ACCOUNT(inf):
+    username,password=inf
+    print(username,password)
+    return
+def ERROR_CATCH(id):
+    errmsgs=[
+        'Unknown error',
+        'Unsupported opcode from client'
+    ]
+    print(str('Error #'+str(id)+'|'+errmsgs[id]+'\n'))
+    return
 
-#
+recv_opcode=8
+recv_operand=('John Smith','password123')
+
+REGISTER_ACCOUNT(recv_operand) if recv_opcode==0 else LOGIN_ACCOUNT(recv_operand) if recv_opcode==1 else ERROR_CATCH(1)
+
+
