c6b9349a02b1d99da43e302876d626d567409648
samrussell-code <66648381+samrussell-code@users.noreply.github.com>
2021-10-20 09:48:46 +0100

    database script working

--- A-Level Project/1_launch_menu/account_server.py
+++ A-Level Project/1_launch_menu/account_server.py
@@ -1,17 +1,29 @@
 import sqlite3
-
+def dbCreateTable(connection,create_table_sql):#only run this to create a new table in the db
+    cursor=connection.cursor()
+    cursor.execute(create_table_sql)
+def dbAddToTable(connection,sql_insert,values):
+    cursor=connection.cursor()
+    cursor.execute(sql_insert,values)
+    connection.commit()
+    print(cursor.rowcount,'record inserted')
+sql_create_profiles_table="""CREATE TABLE IF NOT EXISTS profiles (
+    id integer PRIMARY KEY,
+    username text NOT NULL,
+    password text NOT NULL
+);"""
 def dbConnect(filename):
     print('Connecting to',filename,'...')
     connection=sqlite3.connect(str(filename+'.db'))
     return connection
-
-def dbCreateTable(connection,create_table_sql):#only run this to create a new table in the db
+def REGISTER_ACCOUNT(username,password,connection):
     cursor=connection.cursor()
-    cursor.execute(create_table_sql)
+    cursor.execute(f'''SELECT id FROM profiles WHERE username="{username}"''')
+    result=cursor.fetchall();print(result)
+    ERR_CATCH(2) if len(result)!=0 else dbAddToTable(connection,'INSERT INTO profiles (username, password) VALUES (?,?);',(username,password))
 
-def REGISTER_ACCOUNT(username,password,cursor):
     return
-def LOGIN_ACCOUNT(username,password,cursor):
+def LOGIN_ACCOUNT(username,password,connection):
     return
 def ERR_CATCH(id):
     errmsgs=[
@@ -23,13 +35,7 @@ def ERR_CATCH(id):
     print(str('Error #'+str(id)+'|'+errmsgs[id]+'\n'))
     return
 
-recv_opcode,recv_operand=0,('John Smith','password123')#temporary data
+recv_opcode,recv_operand=0,('John Smithus','password123')#temporary data
 (username,password),connection=recv_operand,dbConnect('ACCOUNTS')
-sql_create_accounts_table="""CREATE TABLE IF NOT EXISTS profiles (
-    id integer PRIMARY KEY,
-    username text NOT NULL,
-    password text NOT NULL
-);"""
-
-dbCreateTable(connection, sql_create_accounts_table) if connection is not None else ERR_CATCH(3)
+dbCreateTable(connection, sql_create_profiles_table) if connection is not None else ERR_CATCH(3)
 REGISTER_ACCOUNT(username,password,connection) if recv_opcode==0 else LOGIN_ACCOUNT(username,password,connection) if recv_opcode==1 else ERR_CATCH(1)
