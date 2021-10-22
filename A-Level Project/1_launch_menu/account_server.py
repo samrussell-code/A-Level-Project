59138e149d8db0e7877b364252ce041d97de0660
samrussell-code <66648381+samrussell-code@users.noreply.github.com>
2021-10-22 15:49:28 +0100

    wording

--- A-Level Project/1_launch_menu/account_server.py
+++ A-Level Project/1_launch_menu/account_server.py
@@ -1,12 +1,12 @@
 #Server starts, in multiple threads. Each thread can be indicated by the opcode sent before the data, from the client.
 #When the client is at the launcher screen, opcodes will either be REGISTER_ACCOUNT(00) or LOGIN_ACCOUNT(01). The server needs to read the opcode, then interpret the following data correctly.
 
-#example data would be '00' and then, seperately, ['John Smith','password123']. First, tuple[0] is taken, and checked against every database username to see if the username has been taken or not.
+#example data would be '0' and then, seperately, ['John Smith','password123']. First, tuple[0] is taken, and checked against every database username to see if the username has been taken or not.
 #if it is taken, the server will return '0' to signify an unsuccessful registration.
 #if not, the user registration is given its own unique index (previous index+1), where tuple[0] is registered in the database as a username, and tuple[1] is registered as a password. returning '1'
 #to signify a successful registration.
 
-#example data would be '01' and then, seperately, ['John Smith','password123']. First, tuple[0] is taken and checked against every database username. Return '0' if username is not in database.
+#example data would be '1' and then, seperately, ['John Smith','password123']. First, tuple[0] is taken and checked against every database username. Return '0' if username is not in database.
 #Return '1' if password does not match correct password.
 #If the username and password are correct, details must be fetched back to fill out variables for the Account client class.
 import sqlite3, socket, threading
@@ -58,7 +58,7 @@ def NewServer():
     server.bind((host,port))
     return server
 
-class ClientHandler:
+class ClientLogOnHandler:
     def __init__(self,socketInfo,socketObject):
         self.socketObject=socketObject
         self.StartThread(socketInfo)
@@ -80,6 +80,6 @@ while True:
     server.listen()
     socketObject,socketInfo=server.accept()
     if socketObject not in connections:  
-        connections.update({socketObject:ClientHandler(socketInfo,socketObject)})
+        connections.update({socketObject:ClientLogOnHandler(socketInfo,socketObject)})
 
 
